02ef17bee38b438d17a1e30f935e55ee4262652b
Mar Potichkin <marpotichkin@gmail.com>
2026-02-22 23:38:46 -0300

    feat: queryProvider

---
+++ src/providers/QueryProvider.tsx
@@ -0,0 +1,23 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { PropsWithChildren } from "react";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 1,
+            refetchOnWindowFocus: false,
+            staleTime: 1000 * 60 * 5,
+        },
+        mutations: {
+            retry: 0,
+        }
+    }
+})
+
+export function QueryProvider({ children }: PropsWithChildren) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+}
